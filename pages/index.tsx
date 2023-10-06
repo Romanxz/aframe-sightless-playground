181d34225aa0fb1e2210e6441cd9b4244e76d6a4
Roman <technopriest666@gmail.com>
2023-10-06 15:43:10 +0300

    start Sounds Button reimagined

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,7 @@ import dynamic from 'next/dynamic';
 const Playground = dynamic(() => import('../imports/aframe/playground'), { ssr: false })
 
 export default function Page() {
-  const [run, setRun] = React.useState(false);
   return (<ChakraProvider>
-    {run ? <Playground /> :
-      <Stack height="100vh" width="100vw" align="center" justify="center" backgroundColor="gray.50">
-        <Button
-          onClick={() => setRun(true)}
-          colorScheme="blue"
-          size="lg"
-        >
-          Run
-        </Button>
-      </Stack>
-    }
+    <Playground />
   </ChakraProvider>);
 }
