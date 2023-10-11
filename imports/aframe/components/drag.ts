503885ccba65b4a249d0849fedc9bba7a4e2fc1a
Roman Gorbunov <technopriest666@gmail.com>
2023-10-11 14:34:55 +0000

    finally full working drag?

--- imports/aframe/components/drag.ts
+++ imports/aframe/components/drag.ts
@@ -4,8 +4,10 @@ export { };
 AFRAME.registerComponent('drag', {
   init: function () {
     this.intersectedObject = null; // Reference to the intersected object
-    this.startPosition = new THREE.Vector3(); // Initial position of the controller
-    this.startRotation = new THREE.Quaternion(); // Initial rotation of the controller
+    this.distanceToTarget = 0;
+    this.distanceOffset = -38; // Set the desired distance offset in degrees
+    // this.startPosition = new THREE.Vector3(); // Initial position of the controller
+    // this.startRotation = new THREE.Quaternion(); // Initial rotation of the controller
     this.el.addEventListener('triggerdown', this.onTriggerDown.bind(this)); // Listen for triggerdown event
     this.el.addEventListener('triggerup', this.onTriggerUp.bind(this)); // Listen for triggerup event
     this.el.addEventListener('raycaster-intersection', this.onRaycasterIntersection.bind(this)); // Listen for raycaster-intersection event
@@ -24,14 +26,15 @@ AFRAME.registerComponent('drag', {
   onRaycasterIntersectionCleared: function (event) {
     if (!this.isDrag) {
       this.intersectedObject = null; // Clear the intersected object
+      this.distanceToTarget = 0;
       console.log("raycaster-intersection-cleared: ", event);
     }
   },
 
   onTriggerDown: function (event) {
     if (this.intersectedObject !== null) {
-      this.startPosition.copy(this.el.object3D.position); // Save the initial position of the object
-      this.startRotation.copy(this.el.object3D.quaternion); // Save the initial rotation of the controller
+      // this.startPosition.copy(this.el.object3D.position); // Save the initial position of the object
+      // this.startRotation.copy(this.el.object3D.quaternion); // Save the initial rotation of the controller
       this.isDrag = true; // Flag to indicate that dragging has started
       console.log("triggerdown: ", this.startPosition, this.startRotation);
     }
@@ -40,24 +43,31 @@ AFRAME.registerComponent('drag', {
   onTriggerUp: function (event) {
     this.isDrag = false; // Flag to indicate that dragging has ended
     this.intersectedObject = null; // Clear the intersected object
+    this.distanceToTarget = 0;
     console.log("triggerup: ", event);
   },
 
   tick: function () {
     if (this.intersectedObject !== null && this.isDrag) {
       // Get the position and rotation of the controller
+
       const currentControllerPosition = this.el.object3D.getWorldPosition(new THREE.Vector3());
       const currentControllerRotation = this.el.object3D.getWorldQuaternion(new THREE.Quaternion());
-      
+
       // Calculate the offset in the controller's local space based on the initial distance to the target
-      const localOffset = new THREE.Vector3(0, 0, -this.distanceToTarget);
-      
+      const localOffset = new THREE.Vector3(0, 0, -this.distanceToTarget).applyAxisAngle(
+        new THREE.Vector3(1, 0, 0),
+        THREE.MathUtils.degToRad(this.distanceOffset)
+      );
+
+      // Apply the rotation offset to the local offset
+
       // Rotate the local offset to match the controller's current rotation
       localOffset.applyQuaternion(currentControllerRotation);
-      
+
       // Compute the new position of the target entity by adding the rotated offset to the current controller's position
       const newPosition = currentControllerPosition.add(localOffset);
-      
+
       // Update the position of the target entity
       this.intersectedObject.object3D.position.copy(newPosition);
     }
