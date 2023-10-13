2c9dc4e77de7a6d531ca96c36ad3e3bf607a226a
Roman Gorbunov <technopriest666@gmail.com>
2023-10-13 17:00:36 +0000

    drag 18 worldPosition

--- imports/aframe/components/drag.ts
+++ imports/aframe/components/drag.ts
@@ -5,7 +5,7 @@ AFRAME.registerComponent('drag', {
   init: function () {
     this.intersectedObject = null; // Reference to the intersected object
     this.distanceToTarget = 0;
-    this.distanceOffset = -37; // Set the desired distance offset in degrees
+    this.axisAngleOffset = -37; // Set the desired distance offset in degrees
     // this.startPosition = new THREE.Vector3(); // Initial position of the controller
     // this.startRotation = new THREE.Quaternion(); // Initial rotation of the controller
     this.el.addEventListener('triggerdown', this.onTriggerDown.bind(this)); // Listen for triggerdown event
@@ -35,7 +35,7 @@ AFRAME.registerComponent('drag', {
     if (this.intersectedObject !== null) {
       // this.startPosition.copy(this.el.object3D.position); // Save the initial position of the object
       // this.startRotation.copy(this.el.object3D.quaternion); // Save the initial rotation of the controller
-      this.distanceToTarget = this.intersectedObject.object3D.position.distanceTo(this.el.object3D.position);
+      this.distanceToTarget = this.intersectedObject.object3D.getWorldPosition(new THREE.Vector3()).distanceTo(this.el.object3D.position);
       this.isDrag = true; // Flag to indicate that dragging has started
       console.log("triggerdown: ", this.startPosition, this.startRotation);
     }
@@ -53,14 +53,14 @@ AFRAME.registerComponent('drag', {
       // Get the position and rotation of the controller
       const controllerPosition = this.el.object3D.getWorldPosition(new THREE.Vector3());
       const controllerRotation = this.el.object3D.getWorldQuaternion(new THREE.Quaternion());
-      // Calculate the offset in the controller's local space based on the initial distance to the target
-      const localOffset = new THREE.Vector3(0, 0, -this.distanceToTarget)
+      // Calculate the offset in the controller's world space based on the initial distance to the target
+      const raduisOffset = new THREE.Vector3(0, 0, -this.distanceToTarget)
         // Apply the rotation offset to the local offset
-        .applyAxisAngle(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(this.distanceOffset));
+        .applyAxisAngle(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(this.axisAngleOffset));
       // Rotate the local offset to match the controller's current rotation
-      localOffset.applyQuaternion(controllerRotation);
+      raduisOffset.applyQuaternion(controllerRotation);
       // Compute the new position of the target entity by adding the rotated offset to the current controller's position
-      const newPosition = controllerPosition.add(localOffset);
+      const newPosition = controllerPosition.add(raduisOffset);
       // Update the position of the target entity
       this.intersectedObject.object3D.position.copy(newPosition);
     }
