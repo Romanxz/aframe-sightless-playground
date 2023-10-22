f820c9a5ab0bb34fde07fa263a7be396b3c74dff
Roman Gorbunov <technopriest666@gmail.com>
2023-10-22 15:31:07 +0000

    geometry-connector links array

--- imports/aframe/components/geometry-connector.ts
+++ imports/aframe/components/geometry-connector.ts
@@ -1,16 +1,16 @@
 // @ts-nocheck
 export { };
+import { uuid as uuidv4 } from 'uuid';
 
 AFRAME.registerComponent("geometry-connector", {
   init: function () {
     this.sceneContent = document.getElementById("content");
-    this.isThumbstickReleased = true; // Flag to track if the thumbstick is released
     this.distanceToTarget = 0.3 // Distance between the controller and the sphere entity
     this.axisAngleOffset = -37; // Set the desired angle offset in degrees
-    this.linkEntity = null; // Reference to the linkEntity entity
     this.intersectedObject = null; // Reference to the intersected object
     this.lastIntersectedObject = null; // Reference to the last intersected object
-    this.linkTargetPosition = null; 
+    this.linkId = null;
+    this.links = {};
     // Event listeners for Oculus Touch controller
     // this.el.addEventListener('thumbstickmoved', this.onThumbStickMoved.bind(this));
     this.el.addEventListener("thumbstickdown", this.onThumbStickDown.bind(this));
@@ -37,59 +37,66 @@ AFRAME.registerComponent("geometry-connector", {
 
   onThumbStickDown: function (event) {
     if (this.intersectedObject !== null && this.linkEntity === null) {
-      // Create the geometry entity with the specified properties
-      this.linkEntity = document.createElement("a-entity");
-      this.linkEntity.setAttribute("geometry", { primitive: "cylinder", radius: 0.005, segmentsHeight: 6, openEnded: true });
-      // Add the geometry entity as a child of the sceneContent entity
-      this.sceneContent.appendChild(this.linkEntity);
-      const targetWorldPosition = this.intersectedObject.object3D.getWorldPosition(new THREE.Vector3());
+      // Create the link entity with the specified properties
+      this.linkId = uuid();
+      this.links[this.linkId].entity = document.createElement("a-entity");
+      this.links[this.linkId].entity.setAttribute("geometry", { primitive: "cylinder", radius: 0.005, segmentsHeight: 6, openEnded: true });
+      // Add the link entity as a child of the sceneContent entity
+      this.sceneContent.appendChild(this.links[this.linkId].entity);
+      this.links[this.linkId].source = this.intersectedObject;
+      const intersectedObjecttWorldPosition = this.intersectedObject.object3D.getWorldPosition(new THREE.Vector3());
       const controllerWorldPosition = this.el.object3D.getWorldPosition(new THREE.Vector3());
-      this.distanceToTarget = targetWorldPosition.distanceTo(controllerWorldPosition);
+      this.distanceToTarget = intersectedObjecttWorldPosition.distanceTo(controllerWorldPosition);
       this.lastIntersectedObject = this.intersectedObject;
       this.el.components.haptics.pulse(0.8, 80);
     };
   },
 
   onThumbStickUp: function () {
-    if ((this.intersectedObject !== this.lastIntersectedObject || null) && this.linkEntity !== null) {
-      this.linkTargetPosition = this.intersectedObject.object3D.position;
+    if ((this.intersectedObject !== this.lastIntersectedObject || null) && this.links[this.linkId] !== undefined) {
+      this.links[this.linkId].target = this.intersectedObject;
+      this.linkId = null;
       this.el.components.haptics.pulse(0.6, 200);
     } else {
-      this.sceneContent.removeChild(this.linkEntity);
-      this.linkEntity = null;
+      this.sceneContent.removeChild(this.links[this.linkId].entity);
+      this.links[this.linkId] = undefined;
+      this.linkId = null;
       this.el.components.haptics.pulse(0.8, 80);
     }
   },
 
   tick: function () {
-    if (this.linkEntity !== null && this.lastIntersectedObject !== null) {
-      const linkSourcePosition = this.lastIntersectedObject.object3D.position;
-      // Get the position and rotation of the controller
-      const controllerPosition = this.el.object3D.getWorldPosition(new THREE.Vector3());
-      const controllerRotation = this.el.object3D.getWorldQuaternion(new THREE.Quaternion());
-      // Calculate the offset in the controller's world space based on the initial distance to the target
-      const raduisOffset = new THREE.Vector3(0, 0, -this.distanceToTarget)
-        // Apply the rotation offset to the local offset
-        .applyAxisAngle(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(this.axisAngleOffset));
-      // Rotate the local offset to match the controller's current rotation
-      raduisOffset.applyQuaternion(controllerRotation);
-      // Compute the new position of the target entity inside parent's local space by adding the rotated offset to the current controller's position
-      const linkTargetPosition = this.linkTargetPosition !== null 
-      ? this.linkTargetPosition 
-      : this.sceneContent.object3D.worldToLocal(controllerPosition.add(raduisOffset), new THREE.Vector3());
+    if (Object.keys(this.links).length > 0) {
+      for (const link in this.links) {
+        const linkSourcePosition = link.source.object3D.position;
+        let linkTargetPosition;
+        if (!link.target) {
+          // Get the position and rotation of the controller
+          const controllerPosition = this.el.object3D.getWorldPosition(new THREE.Vector3());
+          const controllerRotation = this.el.object3D.getWorldQuaternion(new THREE.Quaternion());
+          // Calculate the offset in the controller's world space based on the initial distance to the target
+          const raduisOffset = new THREE.Vector3(0, 0, -this.distanceToTarget)
+            // Apply the rotation offset to the local offset
+            .applyAxisAngle(new THREE.Vector3(1, 0, 0), THREE.MathUtils.degToRad(this.axisAngleOffset));
+          // Rotate the local offset to match the controller's current rotation
+          raduisOffset.applyQuaternion(controllerRotation);
+          // Compute the new position of the target entity inside parent's local space by adding the rotated offset to the current controller's position
+          linkTargetPosition = this.sceneContent.object3D.worldToLocal(controllerPosition.add(raduisOffset), new THREE.Vector3());
+        } else linkTargetPosition = link.target.object3D.position;
+        
+        const linkMidpoint = new THREE.Vector3().addVectors(linkSourcePosition, linkTargetPosition).multiplyScalar(0.5);
 
-      const linkMidpoint = new THREE.Vector3().addVectors(linkSourcePosition, linkTargetPosition).multiplyScalar(0.5);
+        const linkHeight = linkSourcePosition.distanceTo(linkTargetPosition);
 
-      const linkHeight = linkSourcePosition.distanceTo(linkTargetPosition);
-
-      let linkOrientation = new THREE.Quaternion().setFromUnitVectors(
-        new THREE.Vector3(0, 1, 0),
-        new THREE.Vector3().subVectors(linkTargetPosition, linkSourcePosition).normalize()
-      );
-      // Update the coordinates of the link entity
-      this.linkEntity.object3D.position.copy(linkMidpoint);
-      this.linkEntity.object3D.setRotationFromQuaternion(linkOrientation);
-      this.linkEntity.setAttribute('geometry', { height: linkHeight });
+        let linkOrientation = new THREE.Quaternion().setFromUnitVectors(
+          new THREE.Vector3(0, 1, 0),
+          new THREE.Vector3().subVectors(linkTargetPosition, linkSourcePosition).normalize()
+        );
+        // Update the coordinates of the link entity
+        link.entity.object3D.position.copy(linkMidpoint);
+        link.entity.object3D.setRotationFromQuaternion(linkOrientation);
+        link.entity.setAttribute('geometry', { height: linkHeight });
+      }
     }
   },
 });
